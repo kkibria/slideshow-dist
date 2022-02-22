b2ddf25ce269267436fc9366ef33cef4719acf8d
Khan Kibria <khan.kibria@gmail.com>
2022-02-21 17:35:09 -0800

    updated to fix publish problems with files embeded in pkg binary

--- deploy.js
+++ deploy.js
@@ -1,102 +1,18 @@
 'use strict';
 
-import { readFile, mkdir } from 'fs/promises';
-import { constants } from 'fs';
-import { default as util } from 'util';
-import { default as path } from 'path';
-import { default as NodeGit } from 'nodegit';
-import { execFile } from 'child_process';
-import { getopt } from 'stdio';
-import { default as copyfiles } from 'copyfiles';
+const publish = require('./lib/publish');
+const { Command } = require('commander');
+const program = new Command();
 
-
-const Reset = "\x1b[0m";
-const FgGreen = "\x1b[32m";
-const FgCyan = "\x1b[36m";
-const Bright = "\x1b[1m";
-const Dim = "\x1b[2m";
-const Normal = "\x1b[22m";
-
-const dist_pfx = "dist/";
-const dst_pfx = "public/";
-const branch = "gh-pages";
-
-async function copy_dirs(spec) {
-    let i;
-    let pr_list = [];
-    let f = util.promisify(copyfiles);
-    for (i in spec) {
-        let paths, opt;
-        [paths, opt] = spec[i];
-        const pr = f(paths, opt);
-        pr_list.push(pr);
-    }
-    await Promise.all(pr_list);
-}
-
-async function publish() {
-    let s = await readFile('package.json');
-    let pkg = JSON.parse(s);
-    let copy_spec = pkg.publishDirs;
-    await copy_dirs(copy_spec);
-}
-
-async function commit(msg) {
-    let docs = path.resolve(dst_pfx);
-    let docsrepo;
-
-    try {
-        docsrepo = await NodeGit.Repository.open(docs);
-        const remote = await docsrepo.getRemote('origin');
-    } catch (e) {
-        const pathToRepo = path.resolve(".");
-        const repo = await NodeGit.Repository.open(pathToRepo);
-        const url = (await repo.getRemote('origin')).url();
-        await mkdir(docs, { recursive: true });
-        docsrepo = await NodeGit.Repository.init(docs, 0);
-        await NodeGit.Remote.create(docsrepo, 'origin', url);
-        await docsrepo.setHead("refs/heads/" + branch);
-    }
-
-    let parent = [];
-    const h = await docsrepo.getHeadCommit();
-    if (h) {
-        parent.push(h);
-    }
-    const index = await docsrepo.refreshIndex();
-    await index.addAll(".");
-    await index.write();
-    const oid = await index.writeTree();
-    const sig = NodeGit.Signature.now("default", "default");
-    await docsrepo.createCommit('HEAD', sig, sig, msg, oid, parent);
-    await push();
-}
-
-async function push() {
-    const f = util.promisify(execFile);
-    const { stdout, stderr } = await f('git', ['push', '-f', 'origin', branch], { cwd: dst_pfx });
-    console.log(stdout);
-    console.log(stderr);
-}
-
-async function deploy(bCommit) {
-    console.log(FgCyan + '\n:: Deploying from "public" to "gh-pages"' + Reset);
-    let start = new Date();
-    await publish();
-    if (bCommit) {
-        console.log(FgGreen + '\n:: Pushing to remote.' + Reset);
-        await commit("publish");
-    }
-    let end = new Date() - start;
-    console.log(FgGreen + '\n:: Deploy completed in %dms.' + Reset, end);
-}
-
-try {
-    const ops = getopt({
-        'push': { key: 'p', description: 'push to remote git' },
+program
+    .name('deploy')
+    .option('-p, --push', 'push to remote git', false)
+    .action(async (ops) => {
+        try {
+            await publish('md', ".", ops.push);
+        } catch (e) {
+            console.error(`error: ${e}`);
+        }
     });
-    console.log("options: ", ops);
-    deploy(ops.push);
-} catch (e) {
-    console.error("error.");
-}
+
+program.parse();
--- index.js
+++ index.js
@@ -1,70 +1,37 @@
 'use strict';
 
-import { default as sirv } from 'sirv';
-import { default as polka } from 'polka';
-import { default as compression } from 'compression';
-import { createServer } from 'livereload';
-import { dirname } from 'path';
-import { fileURLToPath } from 'url';
-import { find } from 'port-authority';
-import { readFile, writeFile, copyFile, mkdir } from 'fs/promises';
-import { getopt } from 'stdio';
+const publish = require('./lib/publish');
+const slideshow = require('./lib/slideshow');
+const pkgjson = require('./package.json');
+const { Command } = require('commander');
+const program = new Command();
 
-const __dirname = dirname(fileURLToPath(import.meta.url));
-const Reset = "\x1b[0m";
-const FgGreen = "\x1b[32m";
-const FgCyan = "\x1b[36m";
-const Bright = "\x1b[1m";
-const Dim = "\x1b[2m";
-const Normal = "\x1b[22m";
+program
+    .name('slideshow')
+    .description('Easy slideshow presentation in browser')
+    .version(pkgjson.version);
 
-function inject(port) {
-    const snippetSrc = `'//' + (self.location.host || 'localhost').split(':')[0] + ':${port}/livereload.js?snipver=1'`;
-    return `(function(l, r) { if (!l || l.getElementById('livereloadscript')) return; r = l.createElement('script'); r.async = 1; r.src = ${snippetSrc}; r.id = 'livereloadscript'; l.getElementsByTagName('head')[0].appendChild(r) })(self.document);`
-}
-
-async function start(liveport, srvrport, mdpath) {
-    const port = await find(liveport);
-    var server = createServer({
-        port: port,
-        extraExts: ['md']
+program.command('open')
+    .description('Starts slideshow server to open presentation')
+    .argument('<mdpath>', 'directory/path to markdown slideshow file/s')
+    .action(async (mdpath) => {
+        try {
+            await slideshow(mdpath);
+        } catch (e) {
+            console.error(`error: ${e}`);
+        }
     });
-    server.watch(__dirname + "/" + mdpath);
 
-    // now instrument the code
-    await mkdir("dev/build", { recursive: true });
-    const src = "dist/bundle";
-    const dst = "dev/build/bundle";
-    const bundle = await readFile(src + '.js');
-    await writeFile(dst + '.js', inject(port) + '\n' + bundle);
-    await copyFile(src + '.css', dst + '.css');
-    console.log(`> ${FgGreen}livereload${Reset} listening to ${FgCyan}port ${port}${Reset}`);
-
-    const assets = sirv('dev');
-    const md = sirv(mdpath, { dev: true });
-    const img = sirv(mdpath + "/images", { dev: true });
-    const polkport = await find(srvrport);
-    polka()
-        .use(compression(), assets)
-        .use('/md', compression(), md)
-        .use('/images', compression(), img)
-        .listen(polkport, err => {
-            if (err) throw err;
-            console.log(`> ${FgGreen}Server${Reset} ready at ${FgCyan}http://localhost:${polkport}${Reset}`);
-            console.log(`\n===== ${FgGreen}Use you browser to view${Reset} =====`);
-        });
-}
-
-try {
-    const ops = getopt({
-        'mddir': {
-            key: 'm',
-            description: 'directory of the markdown files',
-            required: true,
-            args: 1
-        },
+program.command('publish')
+    .description('Package all the files in a directory named "public" for website')
+    .argument('<mdpath>', 'directory/path to markdown slideshow file/s')
+    .option('--out <string>', 'directory where "public" will be placed')
+    .action(async (mdpath, options) => {
+        try {
+            await publish(mdpath, options.out, false);
+        } catch (e) {
+            console.error(`error: ${e}`);
+        }
     });
-    start(35729, 5000, ops.mddir);
-} catch (e) {
-    console.error("error.");
-}
+
+program.parse();
